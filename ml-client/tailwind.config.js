f06ccb8505eaa5460055b0899ed0ca831307fa2d
curibe <cesar@monadical.com>
2022-11-20 18:37:41 -0500

    front: use google fonts open sans

--- ml-client/tailwind.config.js
+++ ml-client/tailwind.config.js
@@ -4,7 +4,9 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: { 'open-sans': ['Poppins', 'sans-serif'] }
+    },
   },
   plugins: [],
 }

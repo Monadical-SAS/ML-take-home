275ac8919add481fabfe0be2999d5b854aa33f79
curibe <cesar@monadical.com>
2022-11-20 18:36:09 -0500

    backend: allow to return full result as list

--- ml-server/app/models/response.py
+++ ml-server/app/models/response.py
@@ -1,9 +1,9 @@
-from typing import Any
+from typing import Any, List
 
 from app.models.schemas.classifier import ClassifierResponseSchema
 
 
 def ResponseModel(
-    *, message: str, success=True, data: ClassifierResponseSchema = None
+    *, message: str, success=True, data: List[ClassifierResponseSchema] = None
 ) -> dict:
     return {"success": success, "message": message, "data": data}

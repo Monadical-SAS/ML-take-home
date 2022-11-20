498d7cba6b0c1e4f3caffb96e86a035c9b153b38
curibe <cesar@monadical.com>
2022-11-20 18:38:41 -0500

    update classifier component

--- ml-client/src/components/Classifier/Classifier.tsx
+++ ml-client/src/components/Classifier/Classifier.tsx
@@ -1,15 +1,20 @@
 import { Fragment } from "react";
+import Footer from "../Footer/Footer";
 import ImageLoader from "../ImageLoader/ImageLoader";
+import NavBar from "../Navbar/NavBar";
 
 function Classifier() {
   return (
-    <div className="flex flex-col justify-center bg-slate-200 h-screen">
-        <h1 className="flex text-6xl text-blue-800 font-bold my-8 justify-center">
-        Image classifier
+    <>
+      <NavBar />
+      <div className="flex flex-col bg-slate-200 w-full h-[82vh]">
+        <h1 className="flex text-6xl font-open-sans font-medium text-sky-700 my-8 justify-center">
+          Image classifier
         </h1>
-        <ImageLoader/>
-
-    </div>
+        <ImageLoader />
+      </div>
+      <Footer />
+    </>
   );
 }
 

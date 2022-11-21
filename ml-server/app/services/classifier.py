0f1d52c0807347dd88aa7ac883a9238e0ef617bd
juanArias8 <ariasg.juandavid@gmail.com>
2022-11-21 18:11:31 -0500

    save vit base model when running the server at first time

--- ml-server/app/services/classifier.py
+++ ml-server/app/services/classifier.py
@@ -1,5 +1,5 @@
 from typing import Any, List, Union
-
+from pathlib import Path
 from PIL import Image
 from transformers import pipeline
 
@@ -7,12 +7,17 @@ from app.models.schemas.classifier import ClassifierResponseSchema
 
 imageType = Union[Image.Image, Any]
 
+MODEL_BASE = "google/vit-base-patch16-224-in21k"
 MODEL_NAME = "./mlmodels/vit-potatoes-plant-health-status/"
 
 
 class ImageClassifier:
     def __init__(self):
-        self.classifier = pipeline("image-classification", model=MODEL_NAME)
+        self.model_name = MODEL_NAME if Path(MODEL_NAME).exists() else MODEL_BASE
+        self.classifier = pipeline("image-classification", model=self.model_name)
+
+        if not Path(MODEL_NAME).exists():
+            self.classifier.save_pretrained(MODEL_NAME)
 
     def predict(self, image: imageType) -> List[ClassifierResponseSchema]:
         result = self.classifier(image)

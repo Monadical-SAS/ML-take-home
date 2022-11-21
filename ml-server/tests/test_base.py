13ca35cb6d2c557435891b95439480f4ec2b8f33
juanArias8 <ariasg.juandavid@gmail.com>
2022-11-21 18:14:48 -0500

    test classify endpoint

--- ml-server/tests/test_base.py
+++ ml-server/tests/test_base.py
@@ -2,3 +2,14 @@ def test_base(app_client):
     response = app_client.get("/")
     assert response.status_code == 200
     assert response.json()["message"].startswith("Welcome to the image classifier")
+
+
+def test_classify(app_client):
+    response = app_client.post(
+        "/classify",
+        files={"file": open("test_image.jpeg", "rb")},
+    )
+    assert response.status_code == 200
+    assert response.json()["message"] == "Successful classification"
+    assert response.json()["data"][0]["label"] == "healthy"
+    assert response.json()["data"][0]["score"] > 0.9
